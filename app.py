47f89e53f56b73aef74d2a3392d9f08d7f1d081d
Da Lin <lda1@fb.com>
2023-06-17 18:39:36 +0800

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
